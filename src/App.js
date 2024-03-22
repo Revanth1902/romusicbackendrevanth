c7ab9782db3b6c49e2b71c83a3bd9d272f66cbb9
Revanth1902 <revanth19a@gmail.com>
2024-03-22 16:23:29 +0530

    routes done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Dashboard from "./Page/Dashboard";
 import OrderManagement from "./Page/OrderManagementSection/OrderManagement";
 import ProductManagement from "./Page/Product Management/ProductManagement";
 import PaymentsManagementnewproduct from "./Page/Product Management/AddProduct";
-
 import InventoryManagement from "./Page/InventoryMangement/InventoryManagement";
-
 import AgentManagement from "./Page/AgentManagementSection/AgentManagement";
 import UserManagement from "./Page/UserManagement/UserManagement";
-
 import Login from "./Page/Login/loginadmin";
-
 import BannerComponent from "./Page/InventoryMangement/addbaneer";
 import Coupons from "./Page/Coupon Management/couponcode";
 import EditProduct from "./Page/Product Management/editproduct";
@@ -19,43 +15,41 @@ import BrandsManagement from "./Page/InventoryMangement/brandmanagement";
 import CategoryComponent from "./Page/InventoryMangement/CategoryMangement";
 import StaffManagement from "./Page/StaffManagement/Staffmanagement";
 
+// Function to check if the token exists in cookies
+const getTokenFromCookies = () => {
+  // Implement logic to get token from cookies and return it
+  // For example:
+  const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+  return token ? token.split('=')[1] : null;
+};
+
+// Higher-order component to check if the token exists in cookies
+const ProtectedRoute = ({ element }) => {
+  const token = getTokenFromCookies();
+
+  // If token exists, render the provided element, else redirect to login
+  return token ? element : <Navigate to="/loginadmin" />;
+};
+
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/ordermanagement" element={<OrderManagement />}></Route>
-          <Route
-            path="/productmanagement"
-            element={<ProductManagement />}
-          ></Route>
-
-          <Route path="/usermanagement" element={<UserManagement />}></Route>
-
-          <Route path="/agentmanagement" element={<AgentManagement />}></Route>
-          <Route path="/brandmanagement" element={<BrandsManagement />}></Route>
-          <Route path="/couponcodes" element={<Coupons />}></Route>
-          <Route path="/editproduct/:id" element={<EditProduct />} />
-          <Route
-            path="/categorymanagement"
-            element={<CategoryComponent />}
-          ></Route>
-          <Route
-            path="/addproduct"
-            element={<PaymentsManagementnewproduct />}
-          ></Route>
-          <Route path="/addbanner" element={<BannerComponent />}></Route>
-          <Route path="/loginadmin" element={<Login />}></Route>
-          <Route
-            path="/inventorymanagement"
-            element={<InventoryManagement />}
-          ></Route>
-           <Route
-            path="/staffmanagement"
-            element={<StaffManagement />}
-          ></Route>
-          
+          <Route path="/" element={<ProtectedRoute element={<Dashboard />} />} />
+          <Route path="/ordermanagement" element={<ProtectedRoute element={<OrderManagement />} />} />
+          <Route path="/productmanagement" element={<ProtectedRoute element={<ProductManagement />} />} />
+          <Route path="/usermanagement" element={<ProtectedRoute element={<UserManagement />} />} />
+          <Route path="/agentmanagement" element={<ProtectedRoute element={<AgentManagement />} />} />
+          <Route path="/brandmanagement" element={<ProtectedRoute element={<BrandsManagement />} />} />
+          <Route path="/couponcodes" element={<ProtectedRoute element={<Coupons />} />} />
+          <Route path="/editproduct/:id" element={<ProtectedRoute element={<EditProduct />} />} />
+          <Route path="/categorymanagement" element={<ProtectedRoute element={<CategoryComponent />} />} />
+          <Route path="/addproduct" element={<ProtectedRoute element={<PaymentsManagementnewproduct />} />} />
+          <Route path="/addbanner" element={<ProtectedRoute element={<BannerComponent />} />} />
+          <Route path="/loginadmin" element={<Login />} />
+          <Route path="/inventorymanagement" element={<ProtectedRoute element={<InventoryManagement />} />} />
+          <Route path="/staffmanagement" element={<ProtectedRoute element={<StaffManagement />} />} />
         </Routes>
       </BrowserRouter>
     </>
